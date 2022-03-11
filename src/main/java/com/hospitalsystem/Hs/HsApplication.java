7c410ff3fe5ba464c965026dcbca2a71bedf8ee2
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-03-11 10:32:28 +0300

    Shit is crazy

--- src/main/java/com/hospitalsystem/Hs/HsApplication.java
+++ src/main/java/com/hospitalsystem/Hs/HsApplication.java
@@ -2,12 +2,12 @@ package com.hospitalsystem.Hs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class HsApplication {
 
     public static void main(String[] args) {
