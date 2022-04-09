5a51aeb217490f32025de2e4e4be15e626b2aeae
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-04-09 16:36:39 +0300

    Fuckin hate manchester united

--- src/hsfrontend/src/Pharmacy/Sections/Dashboard/Dashboard.jsx
+++ src/hsfrontend/src/Pharmacy/Sections/Dashboard/Dashboard.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Dashboard.css";
+import SectionName from "../../Components/SectionName/SectionName";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const title = {
+    main: "Dashboard",
+    sub: "A quick data overview of the inventory",
+  };
+  return (
+    <div className="Dashboard__container">
+      <div className="Dashboard__top">
+        <SectionName title={title} />
+      </div>
+      <div className="Dashboard__bottom"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
